ad855f85a670aae3c674402d29f00fef00798d4b
iykeafrica <ikechiucheagwu@gmail.com>
2021-08-06 01:39:51 +0100

    Lazy delegate added to Retrofit property and SuccessContributionApi in appcompositionRoot

--- app/src/main/java/com/example/successcontribution/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/example/successcontribution/common/composition/AppCompositionRoot.kt
@@ -10,29 +10,31 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class AppCompositionRoot {
-    private val loggingInterceptor =
-        run {
-            val httpLoggingInterceptor = HttpLoggingInterceptor()
-            httpLoggingInterceptor.apply {
-                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
-            }
+    private val loggingInterceptor = run {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
         }
+    }
 
-    private val httpClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
+    private val httpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
         .connectTimeout(10, TimeUnit.SECONDS)
         .writeTimeout(10, TimeUnit.SECONDS)
         .readTimeout(10, TimeUnit.SECONDS)
         .build()
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(Constant.BASE_URL)
-        .client(httpClient)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-    private val successContributionsApi: SuccessContributionsApi = retrofit.create(
-        SuccessContributionsApi::class.java)
+    }
+
+    private val successContributionsApi: SuccessContributionsApi by lazy {
+        retrofit.create(SuccessContributionsApi::class.java)
+    }
 
     val attemptLoginUseCase: AttemptLoginUseCase = AttemptLoginUseCase(successContributionsApi)
 
